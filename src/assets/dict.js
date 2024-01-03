d5c87b0f592f8f02c1f91006790f4f0baf7e953a
李贺 <1815939234@qq.com>
2024-01-04 01:39:43 +0800

    新增静态数据字典文件

---
+++ src/assets/dict.js
@@ -0,0 +1,15 @@
+// dict.js
+
+// 数据字典
+export const dictionary = {
+    1: '男',
+    2: '女'
+  };
+
+// 朝向
+  export const orientation = ['东', '西', '南', '北']
+
+
+  // 朝向
+  export const roomType = ['单间', '一室户', '主卧一室户', '厅卧一室户']
+    
