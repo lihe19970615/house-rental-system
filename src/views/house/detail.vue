72233cc53bd7750a42f2d7ebbdbe44a1a7715227
李贺 <1815939234@qq.com>
2024-01-04 01:41:08 +0800

    新增房子详情页面

--- src/views/house/detail.vue
+++ src/views/house/detail.vue
@@ -1,10 +1,29 @@
 <template>
-  <div>detail</div>
+  <div>{{house}}</div>
 </template>
 
 <script>
+import { getHouseDetail } from '../../api/house'
 export default {
-
+  name: 'Detail',
+  components: {
+  },
+  data() {
+    return {
+      house: {},
+    };
+  },
+  async mounted(){
+    console.log(this.$route.query.id)
+    const houseId = this.$route.query.id 
+    const res = await getHouseDetail({houseId:houseId})
+    console.log('res',res)  
+    console.log('res.data.data.items',res.data.data.item)
+    this.house = res.data.data.item  
+  },
+  methods: {
+    
+  },
 }
 </script>
 
